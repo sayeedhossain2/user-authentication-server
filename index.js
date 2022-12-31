a5b2fcad3085e6e5387e69e7599da17411048b03
sayeedhossain2 <sayeedh95@gmail.com>
2023-01-01 02:30:29 +0600

    delete and post method done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -17,11 +17,36 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const allCompany = client.db("companyInfo").collection("details");
+
+    // details post
+    app.post("/details", async (req, res) => {
+      const information = req.body;
+      const result = await allCompany.insertOne(information);
+      res.send(result);
+    });
+
+    // details get
+    app.get("/alldetails", async (req, res) => {
+      const query = {};
+      const allInfo = await allCompany.find(query).toArray();
+      res.send(allInfo);
+    });
+
+    // delete method
+    app.delete("/companyInfor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allCompany.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("test task server is running");
